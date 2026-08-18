7dcbe28f71f6979c320b1f06e4aaddb7e88d7350
rafiliano <rafiargantoliano@gmail.com>
2026-08-18 13:16:28 +0700

    Add recommendations and transportations GET endpoints

--- backend/main.py
+++ backend/main.py
@@ -36,6 +36,14 @@ def health():
 def trip_category():
     return {"Backpacker", "Standard", "Luxury"}
 
+@app.get("/api/v1/recommendations")
+def trip_recommendation():
+    return {"Tokyo Tower", "Mount Fuji", "Shibuya"}
+
+@app.get("/api/v1/transportations")
+def trip_transportation():
+    return {"Bus", "Train", "Flight"}
+
 
 # POST endpoint — receives JSON, returns JSON
 @app.post("/api/v1/trips")
